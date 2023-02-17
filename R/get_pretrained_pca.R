ac8f7c04b722e888ad7f418cad274e5bce98af0a
Eirik Sjåvik <Eirik@eduroam-193-157-244-134.wlan.uio.no>
2023-02-17 14:36:55 +0100

    Added prep_demand_temp_data.R for fetching ERA temperature grid and get_pretrained_pca.R for making specific PCA matrices for later use.

---
+++ R/get_pretrained_pca.R
@@ -0,0 +1,48 @@
+#' Gets PCA matrix of temperature data for a specified time period.
+#'
+#' @param file File to store and look for PCA matrix.
+#' @param X_mat ERA temperature matrix.
+#' @param start_train Start date training.
+#' @param stop_train End date training.
+#' @param start_test Start date test.
+#' @param stop_test End date test.
+#' @param NWP NWP matrix.
+#' @param run_again Boolean. If FALSE and file exist function loads pre-made pca matrix.
+#'
+#' @return A
+#' @export
+#'
+#' @examples get_pre_trained_PC_ERA(file='~/Desktop/Master2023/Data/PC_ERA', X_mat,
+#' start_train = "1979-01-01", stop_train="1992-12-31", start_test = "1993-01-01", stop_test="2023-01-31",
+#' NWP=NA,run_again =TRUE)
+#'
+#'
+get_pre_trained_PC_ERA = function(file='~/Desktop/Master2023/Data/PC_ERA', X_mat,
+                                  start_train = '2013-01-01', stop_train='2013-12-31',
+                                  start_test = '2014-01-01', stop_test='2014-12-31',
+                                  NWP=NA,run_again =FALSE){
+
+    train_start = date_demand[(date ==start_train & hour == 1),I]
+    train_stop = date_demand[(date ==stop_train & hour == 24),I]
+    I_train = seq(from = train_start, to = train_stop)
+
+    test_start = date_demand[(date ==start_test & hour == 1),I]
+    test_stop = date_demand[(date ==stop_test & hour == 24),I]
+    I_test = seq(from = (test_start), to =(test_stop))
+
+    name = paste0('PC_ERA_',substring(as.character(year(start_train)),3),'_',
+                  substring(as.character(year(stop_train)),3))
+    file_name = paste0(file,'/',name,'.Rda')
+    print(file_name)
+    if(file.exists(file_name) & run_again ==FALSE){
+        print('Loading PC files')
+        assign(name, get(load(file_name)))
+        return(get(name))
+    }else{
+        print('Creating PC file')
+        out = get_pca(X_mat, I_train, I_test, 2, NWP = NWP)
+        assign(name, out)
+        save(list = name, file = file_name)
+        return (get(name))
+    }
+}
---
+++ R/prep_demand_temp_data.R
@@ -0,0 +1,103 @@
+#' Finds, checks and loads temperature and demand data.
+#'
+#' @param include_na_volume Boolean, include dates with temperature data but na volume data.
+#'
+#' @return Returns a matrix with grid temperature data and a data.table with demand data.
+#' @import ncdf4
+#' @import data.table
+#' @export
+#'
+#' @examples prep = prep_demand_temp_data(include_na_volume = TRUE)
+prep_demand_temp_data = function(include_na_volume = TRUE){
+
+    ##----- 1) Determine which root directory to use -----
+    root_dir_alex = "~/NR/ClimateFutures/RenewableEnergy/Data/DemandForecasting/" ## Alex
+    root_dir_eirik = "/Users/Eirik/Desktop/Master2023/Data/Prob1_data/"           ## Eirik
+
+    if(file.exists(root_dir_alex)){ root_dir = root_dir_alex
+    }else{                          root_dir = root_dir_eirik
+    }
+
+    ##----- 2) Load and extract temperature data -----
+    #f_nc = paste0(root_dir, "/temperature_data.nc4") #Old dataset 2013-2021 only
+    f_nc = paste0(root_dir, "/era_historical_data.nc4") #New dataset 1978-2023
+    nc = nc_open(f_nc)
+    X = ncvar_get(nc, "2m_temperature")
+    hours = 1:24
+    days = as.Date(ncvar_get(nc, "day"), origin = "1970-01-01")
+    nc_close(nc)
+
+    X_temp = matrix(as.vector(X),
+                    nrow = dim(X)[3] * dim(X)[4], #hours times days
+                    ncol = dim(X)[1] * dim(X)[2], #lon time lat
+                    byrow = TRUE)
+
+    dt_date = data.table(expand.grid(hour = hours,date = days)) #track of dates for temp data
+
+    ##----- 3) Load and extract demand volume data -----
+    f_demand = paste0(root_dir, "/nordpool_elspot_volumes.csv") #2013-2021
+    dt_demand = fread(f_demand)
+    dt_demand[, hour := hour + 1]
+
+    ##----- 4) Debug Demand volume (Summer/Winter time problem)-----
+    nas = which(is.na(dt_demand$volume))           #Find missing bc we skip 1 hour ahead -> summertime
+    dt_demand[, rowCount:= .N, by = .(date, hour)] #Find doubles bc we jump back 1 h -> wintertime
+    doubles= which(dt_demand$rowCount >1)[seq(from = 1, to = length(which(dt_demand$rowCount >1)), by = 2)]
+
+    if ( length(nas) == length(doubles) ){ #if data-cutoff in winter
+        volume = dt_demand$volume[-nas]
+        date = dt_demand$date[-doubles]
+        hour = dt_demand$hour[-doubles]
+    } else if ( length(nas) == length(doubles) + 1 ){ #if ata-cutoff in summer
+        volume = dt_demand$volume[-nas]
+        date = dt_demand$date[-c(doubles, length(dt_demand$date))]
+        hour = dt_demand$hour[-c(doubles, length(dt_demand$date))]
+        print("I guess we cut off demand observations sometime during summer time, and didn't go back to winter time")
+    } else{
+        print("We have other sources of na's in the demand data than summer time/winter time")
+    }
+    dt_demand = data.table(volume, date, hour)
+    date_demand = dt_demand[dt_date, on=c("hour", "date")]  #Combine information
+
+    ##----- 5) Debug Temperature Data (X) -----
+    n_nas = rowSums(is.nan(X_temp))
+    if(any(n_nas > 0)){
+        w_missing = which(n_nas > 0)
+        X_mat = X_temp[-w_missing, ]
+        date_demand = date_demand[-w_missing, ]
+    }else{
+        X_mat = X_temp
+    }
+
+
+    if (nrow(date_demand) == nrow(X_mat)){
+        print('We are good to go!')
+    } else if(nrow(X_mat) > nrow(date_demand)){
+        diff = nrow(X_mat)  - nrow(date_demand)
+        diffseq = seq(nrow(X_mat), nrow(X_mat)-diff+1, -1)
+        X_mat = X_mat[-(diffseq),]
+    } else{
+        print('We have insufficient temperature observations.')
+    }
+
+    date_demand[,I:= 1:.N] #set index
+
+    ##----- 6) Do we want to return NA volume observations? -----
+    out = list()
+    if (include_na_volume== FALSE){
+        d_d = date_demand[!is.na(volume)] #Select only rows with volume obs
+        out$X_mat = X_mat[d_d$I,]
+        out$date_demand = d_d[,I:= 1:.N] #reset index
+    } else{
+        out$X_mat = X_mat
+        out$date_demand = date_demand
+    }
+    ##----- 7) Final Check and Print-----
+    if (nrow(out$date_demand) == nrow(out$X_mat)){
+        print(paste0('Demand volume data from ', dt_demand[1,date], ' to ', dt_demand[dim(dt_demand)[1],date],'.' ))
+        print(paste0('Temperature data from ', out$date_demand[1,date], ' to ', out$date_demand[dim(out$date_demand)[1],date],'.'))
+        print(paste0('We have ', dim(out$X_mat)[1], ' hourly observations at ', dim(out$X_mat)[2], ' grid points.'))
+    }
+    return(out)
+}
+
