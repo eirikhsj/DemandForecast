bbf8576090bfea7e893c6ae9203684edb06d828e
Eirik Sjåvik <Eirik@eduroam-193-157-253-172.wlan.uio.no>
2023-02-20 18:33:14 +0100

    Added other get-functions, now for NWP data.

---
+++ R/utils/get_all_NWP_quantiles.R
@@ -0,0 +1,110 @@
+#' Get all NWP quantiles
+#' This is a cover function where one inputs the range of forecasts months.
+#' The function then iterates over the specified range calling get_one_month_NWP_quantiles.R,
+#' and stores the results of these iterated outputs in a data.table.
+#' Output can then be merged with ERA-data using a get_ERA_NWP function to be used in prediction tasks.
+#'
+#' @param path Path to NWP data
+#' @param start_month Start month.
+#' @param start_year Start year.
+#' @param forc_months Number of forecast months from start
+#' @param PC_ERA List of matrices containing PC of ERA tempearture.
+#' @param pc_comp Integer. Number of pca components.
+#' @param reweight Boolean.
+#'
+#' @return Returns quantiles of interest
+#' @export
+#'
+#' @examples get_all_NWP_quantiles(path = "~/Desktop/MasterNR/Prob2_data",
+#' start_month  = '01', start_year = 1993, forc_months = 4,
+#' PC_ERA = PC_ERA_79_92, pc_comp = 1,reweight = TRUE)
+#' get_all_NWP_quantiles(path = "~/Desktop/MasterNR/Prob2_data",
+#' start_month  = '01', start_year = 1993, forc_months = 4,
+#' PC_ERA = PC_ERA_79_92, pc_comp = 1, reweight = FALSE)
+#'
+
+get_all_NWP_quantiles = function(path = "~/Desktop/Master2023/Data/NWP_monthly",
+                                 start_month  = '01', start_year = 1993, forc_months = 355,
+                                 PC_ERA = PC_ERA_79_92, pc_comp = 1,
+                                 reweight = FALSE){
+    #Forc_months gives total forecast months (including start month) .
+
+    files = list.files(path = path, pattern = '*.nc4', full.names=TRUE)
+    num_char = nchar(files[1])
+
+    add_months = forc_months - 1
+
+    #Get dates for data.table
+    first_start_date = as.Date(paste0(start_year, '-', start_month, '-', '01'))
+    add = paste(add_months, 'month')
+    last_start_date = seq(first_start_date, length=2, by=add)[2]
+    init_dates = as.character(seq(first_start_date, length=forc_months, by='1 month'))
+
+    target_hour = rep(c(6,12,18,24), 125)
+
+    #Get indexes for files
+    year_bool = as.numeric(mapply(substring,files, num_char-10, num_char-7)) == start_year
+    month_bool = as.numeric(mapply(substring,files, num_char-5, num_char-4))== as.numeric(start_month)
+    start_files_ix = which((year_bool) & (month_bool))[[1]]
+    stop_files_ix = start_files_ix + forc_months -1
+
+    #Loop over monthly forecast files
+    col_nr = 0
+    NWP_results = list()
+    NWP_results_rew = list()
+    for (file_ix in c(start_files_ix:stop_files_ix)){
+        out = get_one_month_NWP_quantiles(files[file_ix], PC_ERA, pc_comp, rew = reweight)   #Using get_one_month_NWP_quantiles function
+
+        ix = file_ix - start_files_ix + 1
+        print(ix)
+        print(dim(out$NWP_quant_rew))
+        print(dim(out$NWP_quantiles))
+        date_seq = seq(as.Date(init_dates[ix]), length=125, by='1 days')
+        target_date = as.character(rep(date_seq, each=4))
+        init_date = rep(init_dates[ix], 500)
+
+        #Create dt for init month and store
+        temp_dt = data.table(date =as.Date(target_date),
+                             init_date = as.Date(init_date),
+                             hour = target_hour,
+                             out$NWP_quantiles)
+        NWP_results[[file_ix]] = temp_dt
+        NWP_results_rew[[file_ix]] = out$NWP_quant_rew
+    }
+    NWP_all_quantiles = rbindlist(NWP_results)
+
+    if(reweight==TRUE){
+        NWP_all_quantiles_rew = rbindlist(NWP_results_rew)
+    } else{
+        NWP_all_quantiles_rew = NA
+    }
+
+
+    out = c()
+    out$NWP = NWP_all_quantiles
+    out$NWP_rew = NWP_all_quantiles_rew
+    print('Get quantiles completed')
+    return(out)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ R/utils/get_one_month_NWP_quantiles.R
@@ -0,0 +1,160 @@
+#' Get quantiles function
+#'
+#' Finds the correct file of NWP forecast month, gets PCA of forecasts,
+#' and fetches quantiles (0.1-0.9) of 1 selected principle component.
+#' Used in cover-function get_all_NWP_quantiles.R
+#' Specifying reweight = TRUE will also provide reweighted quantiles.
+#'
+#' @param i Integer. File number of forecast month
+#' @param PC_ERA List. The PC_ERA used to get the NWP factor loadings.
+#' @param pc_comp Integer. The principle component of interest for
+#' @param rew Boolean. Whether or not to find reweighted quantiles.
+#' @return Data.table with quantiles 0.1-0.9 and mean of NWP PC of choice.
+#' @export
+#'
+#' @examples files = list.files(path = "~/Desktop/Master2023/Data/NWP_monthly/", pattern = '*.nc4', full.names=TRUE)
+#' get_one_month_NWP_quantiles(files_i = 'Desktop/Master2023/Data/NWP_monthly/forecast_2022_07.nc4', PC_ERA = PC_ERA_79_92, pc_comp = 1)
+#'
+#' @export
+get_one_month_NWP_quantiles= function(files_i = 'Desktop/Master2023/Data/NWP_monthly/forecast_2022_07.nc4',
+                                      PC_ERA, pc_comp, rew= FALSE){
+
+    #Open nc4, assign file and close nc4
+    nc = nc_open(files_i)
+    num_char = nchar(files_i)
+    forec = ncvar_get(nc, attributes(nc$var)$names[1])
+    year_nwp = substring(files_i, num_char-10, num_char-7) #year
+    month_nwp = substring(files_i, num_char-5, num_char-4) #month
+    forc_name = paste0('forc', year_nwp, '_', month_nwp)
+    assign(forc_name, forec)
+    print(c(year_nwp,month_nwp))
+    nc_close(nc)
+
+    #Get forecast and run pca- delete dt when done and store pca-results
+    forecast = get(forc_name)
+    print(dim(forecast))
+    pc_data = get_pca(X_mat, I_train, I_test, 2,
+                      NWP = forecast[,,,1:dim(forecast)[4]],
+                      U = PC_ERA$U,
+                      mu = PC_ERA$mu)
+    rm(forecast)
+    rm(forc_name)
+
+    # Find quantiles
+    mean_pc1 = rowMeans(pc_data$NWP_PC_mat[,pc_comp,])
+    quantiles = t(apply(pc_data$NWP_PC_mat[,pc_comp,], 1, quantile, probs=seq(0.1, 0.9, 0.1)))
+    NWP_quantiles = data.table(quantiles,mean_pc1)
+    names(NWP_quantiles) = c(paste0('NWP_PC',pc_comp, '_q', seq(10, 90, 10)), 'NWP_PC1_mean')
+
+    #Find re-weighted quantiles
+    if (rew == TRUE){
+        #1 get weights
+        #1a find the correct date for ERA
+        init_day = as.Date(paste0(year_nwp,'-',month_nwp,'-01'))
+        date_seq = seq(init_day, length=125, by='1 days')
+        target_date = as.character(rep(date_seq, each=4))
+        reweight_day = as.Date(paste0(year_nwp,'-',month_nwp,'-15'))
+
+        ERA_PC1_15 = PC_ERA$dt_test[(date == reweight_day & hour ==12), get(paste0('PC', pc_comp))]
+        #ERA_PC1_15 = mean(PC_ERA$dt_test[(date == d), get(paste0('PC', pc_comp))])
+        #ERA_PC1_15 = PC_ERA$dt_test[(date == d & hour %in% c(6,12,18,24)), get(paste0('PC', pc_comp))]
+
+        #1b find the matching NWP
+        reweight_results_final = list()
+        #Find weight
+        sq = exp(seq(log(0.000001), log(0.01), length.out = 25))
+        for (k in 1:length(sq)){  #tuning parameter k
+            w = rep(0,dim(pc_data$NWP_PC_mat)[3])
+            for (m in c(57:60)){  # time 57:60 is day 15 hours 6,12,18,24
+                NWP_15 = pc_data$NWP_PC_mat[m,1,]
+                w = w + -0.5*sq[k]*(ERA_PC1_15 - NWP_15)^2
+                #w = w + -0.5*sq[k]*(ERA_PC1_15[m-56] - NWP_15)^2
+            }
+
+            mx = max(w)
+            s = sum( exp(w - mx) )
+            w_n = exp(w - mx) / s
+
+            #Apply weight to remaining obs
+            #When applying weights we are not dealing with actual values,
+            #so we must use a quant est function to get an actual values back
+            reweight_results_temp = list()
+            for (j in 1:500){
+                reweights = t(whdquantile(pc_data$NWP_PC_mat[j,1,], p = seq(0.1, 0.9, 0.1), weights = w_n))
+                reweight_results_temp[[j]] = data.table(reweights)
+            }
+            temp = rbindlist(reweight_results_temp)
+            reweight_results_final[[k]] = data.table(k = rep(k, 500),
+                                                     temp,
+                                                     hour = rep(c(6,12,18,24), 125),
+                                                     init_date = init_day,
+                                                     date = as.Date(target_date))
+
+        }
+
+        NWP_quant_rew = rbindlist(reweight_results_final)
+        names(NWP_quant_rew) = c('Tuning_k', paste0('NWP_PC',pc_comp, '_rew_q', seq(10, 90, 10)), 'hour', 'init_date', 'date')
+    } else{
+        NWP_quant_rew = NA
+    }
+
+    out = c()
+    out$NWP_quant_rew = NWP_quant_rew
+    out$NWP_quantiles = NWP_quantiles
+
+    return(out)
+}
+
+
+#' wquantile.generic
+#' Function which computes the reweighteing of the NWP quantiles.
+#' @param x
+#' @param probs Integer or list of quantiles of interest.
+#' @param cdf.gen
+#' @param weights Add weights.
+#'
+#' @return
+#' @export
+#'
+#' @examples
+wquantile.generic <- function(x, probs, cdf.gen, weights = NA) {
+    n = length(x)
+    if (any(is.na(weights))){weights = rep(1 / n, n)}
+
+    nw = sum(weights)^2 / sum(weights^2) # Kish's effective sample size
+
+    indexes = order(x)
+    x = x[indexes]
+    weights = weights[indexes]
+
+    weights = weights / sum(weights)
+    cdf.probs = cumsum(c(0, weights))
+
+    sapply(probs, function(p) {
+        cdf = cdf.gen(nw, p)
+        q = cdf(cdf.probs)
+        w = tail(q, -1) - head(q, -1)
+        sum(w * x)
+    })
+}
+
+
+#' Title
+#'
+#' @param x
+#' @param probs
+#' @param weights
+#'
+#' @return
+#' @export
+#'
+#' @examples
+whdquantile = function(x, probs, weights = NA) {
+    cdf.gen = function(n, p){
+        func = function(cdf.probs) {
+            pbeta(cdf.probs, (n + 1) * p, (n + 1) * (1 - p))
+        }
+        return(func)
+    }
+    wquantile.generic(x, probs, cdf.gen, weights)
+}
