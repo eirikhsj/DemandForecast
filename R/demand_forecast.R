ed4e2a34ee2019801e5e5c6e210526f237e9f8d4
Eirik Sjåvik <Eirik@eduroam-193-157-177-147.wlan.uio.no>
2023-02-28 21:46:36 +0100

    Changed function order in demand_forecast.R

--- R/demand_forecast.R
+++ R/demand_forecast.R
@@ -36,84 +36,11 @@ demand_forecast = function(X_mat, date_demand, forc_start, forc_end, pred_win = 
     all_days = seq(start, end,  by = '1 days')
     init_days = all_days[mday(all_days)==1]
 
-    Rolling = function(i,X_mat, date_demand, pred_win, pred_lag, train_y,
-                       reg_form, p_comps, other_mods, comb, custom,incl_climatology
-                       ){
 
-        ## ***** Step 1: Form the training and test datasets ****
-        init_day = init_days[i]
-        target_days = seq(init_day+ pred_lag, length.out = pred_win,  by = '1 days')
-        #print(paste('Forecast made on:', init_day))
-
-        train_cutoff = seq(init_day,  length.out = 2, by = paste0('-',train_y, ' year'))[2]
-
-        I_train = date_demand[(date > train_cutoff) & (date < init_day), I]
-        I_test = date_demand[date %in% target_days, I]
-
-        if (p_comps > 0){
-            pc_data  = get_pca(X_mat, I_train, I_test, p_comps) # **** SVD ****
-            dt_train = pc_data$dt_train
-            dt_test  = pc_data$dt_test
-        } else{
-            dt_train = date_demand[I_train, ]
-            dt_test  = date_demand[I_test, ]
-        }
-        #print(paste('We are training on:', dim(dt_train)[1], 'observations'))
-        max_year_train = dt_train[, max(year)]
-        dt_test = dt_test[year > max_year_train, year := max_year_train] #pretend that new year is last year to avoid factor error
-        n = nrow(dt_test)
-
-        ## ***** Step 2: Train models ****
-        mods = list()
-        mods[[1]] = gam(as.formula(reg_form), data = dt_train) #Just Time covariate models
-
-        j = 1
-        if (p_comps > 0){                              #PC GAM MODELS
-            if(custom == FALSE){
-                for (j in 2:(p_comps+1)){
-                    P = paste0("s(PC", 1:(j-1))
-                    PC = paste(P, collapse=') + ')
-                    mods[[j]] = gam(as.formula(sprintf("%s + %s)", reg_form, PC)), data = dt_train)
-                }
-            } else{ mods[[2]] = gam(as.formula(sprintf("%s + %s", reg_form, custom)), data = dt_train) }
-        }
-
-        ## **** Step 3: Check fit ****
-        #print(paste0("Test : ", dt_test[1,date], " - ", dt_test[length(dt_test$date),date], " "))
-        results = dt_test
-        results[,'init_date' := rep(init_day, length(dt_test$date))]
-        results[,'lead_time':= ((as.integer(difftime(date, init_day, units = 'days')))*24)+ hour]
-        for (mod in 1:length(mods)){
-            pred_demand_test = predict(mods[[mod]], newdata = dt_test)
-            RMSE = sqrt(sum((dt_test$volume - pred_demand_test)^2)/n)
-            MAE = sum(abs(dt_test$volume - pred_demand_test))/n
-            #print(sprintf("RMSE mod_%i for initalization %s is %f", mod, format(init_day,"%Y-%m-%d"), RMSE))
-            results[,paste0('pred_',mod) := pred_demand_test]
-        }
-        ## **** Step 4: Climatology ****
-        if(incl_climatology == TRUE){
-            climatology = dt_train[, .(ave_vol = mean(volume)), by = .(month_day = format(date, format ="%m-%d"), hour)]
-
-            if ("02-29" %in% format(dt_test$date, format ="%m-%d") & !("02-29" %in%climatology$month_day)){ #Leap year issue
-                leap = climatology[month_day=="02-28",]
-                leap[,month_day:=  rep("02-29", 24)]
-                climatology = rbind(climatology, leap)
-            }
-            clima_pred = climatology[month_day %in% format(dt_test$date, format ="%m-%d"), ave_vol]
-            results[,'clima_pred' := clima_pred]
-            #print(paste0("Climatology: ", sqrt(sum((dt_test$volume - clima_pred)^2)/n)))
-        }
-
-
-        #detailed_results[[i]] = results
-        #diff = round(difftime(Sys.time(),start_time, units="secs"),4)
-        #print(paste0('This round took ', diff, ' seconds. Only ', round((length(init_days)-i)*diff/60,3), ' minutes left'))
-        return(results)
-    }
-    ## **** Step 5: Run paralell cores ****
+    ## **** Run parallel cores ****
     detailed_results = mclapply(seq_along(init_days),
                       "Rolling",
-                      X_mat = X_mat, date_demand = date_demand,pred_win = pred_win, pred_lag= pred_lag, train_y=train_y,
+                      X_mat = X_mat, date_demand = date_demand,init_days= init_days, pred_win = pred_win, pred_lag= pred_lag, train_y=train_y,
                       reg_form= reg_form, p_comps= p_comps, other_mods= other_mods, comb = comb, custom = custom,
                       incl_climatology = incl_climatology,
                       mc.cores = cores)
@@ -125,4 +52,80 @@ demand_forecast = function(X_mat, date_demand, forc_start, forc_end, pred_win = 
     #out$mods = mods
     print('Demand forecast has completed')
     return(out)
+}
+
+#' @export
+Rolling = function(i,X_mat, date_demand, init_days,pred_win, pred_lag, train_y,
+                   reg_form, p_comps, other_mods, comb, custom,incl_climatology
+){
+
+    ## ***** Step 1: Form the training and test datasets ****
+    init_day = init_days[i]
+    target_days = seq(init_day+ pred_lag, length.out = pred_win,  by = '1 days')
+    #print(paste('Forecast made on:', init_day))
+
+    train_cutoff = seq(init_day,  length.out = 2, by = paste0('-',train_y, ' year'))[2]
+
+    I_train = date_demand[(date > train_cutoff) & (date < init_day), I]
+    I_test = date_demand[date %in% target_days, I]
+
+    if (p_comps > 0){
+        pc_data  = get_pca(X_mat, I_train, I_test, p_comps) # **** SVD ****
+        dt_train = pc_data$dt_train
+        dt_test  = pc_data$dt_test
+    } else{
+        dt_train = date_demand[I_train, ]
+        dt_test  = date_demand[I_test, ]
+    }
+    #print(paste('We are training on:', dim(dt_train)[1], 'observations'))
+    max_year_train = dt_train[, max(year)]
+    dt_test = dt_test[year > max_year_train, year := max_year_train] #pretend that new year is last year to avoid factor error
+    n = nrow(dt_test)
+
+    ## ***** Step 2: Train models ****
+    mods = list()
+    mods[[1]] = gam(as.formula(reg_form), data = dt_train) #Just Time covariate models
+
+    j = 1
+    if (p_comps > 0){                              #PC GAM MODELS
+        if(custom == FALSE){
+            for (j in 2:(p_comps+1)){
+                P = paste0("s(PC", 1:(j-1))
+                PC = paste(P, collapse=') + ')
+                mods[[j]] = gam(as.formula(sprintf("%s + %s)", reg_form, PC)), data = dt_train)
+            }
+        } else{ mods[[2]] = gam(as.formula(sprintf("%s + %s", reg_form, custom)), data = dt_train) }
+    }
+
+    ## **** Step 3: Check fit ****
+    #print(paste0("Test : ", dt_test[1,date], " - ", dt_test[length(dt_test$date),date], " "))
+    results = dt_test
+    results[,'init_date' := rep(init_day, length(dt_test$date))]
+    results[,'lead_time':= ((as.integer(difftime(date, init_day, units = 'days')))*24)+ hour]
+    for (mod in 1:length(mods)){
+        pred_demand_test = predict(mods[[mod]], newdata = dt_test)
+        RMSE = sqrt(sum((dt_test$volume - pred_demand_test)^2)/n)
+        MAE = sum(abs(dt_test$volume - pred_demand_test))/n
+        #print(sprintf("RMSE mod_%i for initalization %s is %f", mod, format(init_day,"%Y-%m-%d"), RMSE))
+        results[,paste0('pred_',mod) := pred_demand_test]
+    }
+    ## **** Step 4: Climatology ****
+    if(incl_climatology == TRUE){
+        climatology = dt_train[, .(ave_vol = mean(volume)), by = .(month_day = format(date, format ="%m-%d"), hour)]
+
+        if ("02-29" %in% format(dt_test$date, format ="%m-%d") & !("02-29" %in%climatology$month_day)){ #Leap year issue
+            leap = climatology[month_day=="02-28",]
+            leap[,month_day:=  rep("02-29", 24)]
+            climatology = rbind(climatology, leap)
+        }
+        clima_pred = climatology[month_day %in% format(dt_test$date, format ="%m-%d"), ave_vol]
+        results[,'clima_pred' := clima_pred]
+        #print(paste0("Climatology: ", sqrt(sum((dt_test$volume - clima_pred)^2)/n)))
+    }
+
+
+    #detailed_results[[i]] = results
+    #diff = round(difftime(Sys.time(),start_time, units="secs"),4)
+    #print(paste0('This round took ', diff, ' seconds. Only ', round((length(init_days)-i)*diff/60,3), ' minutes left'))
+    return(results)
 }
