637bc5b721bc95350a51c810f1eae1459d2c639b
Eirik Sjåvik <Eirik@eduroam-193-157-170-228.wlan.uio.no>
2023-03-20 09:20:37 +0100

    New mods in run_demand.R

--- Dev/run_demand.R
+++ Dev/run_demand.R
@@ -36,13 +36,45 @@ date_demand[,season2 := round(sin(2*pi * season/4), digits = 7)]
 
 date_demand[,hourly_mean_grid := rowMeans(X_mat)]
 
+# for (i in 3:10){
+#
+#     mod = demand_forecast(X_mat, date_demand,forc_start = '2016-01-01', forc_end = '2023-01-01',
+#                                       pred_win = 30, pred_lag = 15, train_y = 5, p_comps = i,  no_pc = TRUE,
+# custom = paste0("as.factor(hour):as.factor(week)  + as.factor(w_day):as.factor(week) + s(week) + s(month) + season1 + season2 + year + s(PC1)+ s(PC",i,")") ,
+# cores = 48, reg_form = "volume~1")
+#     assign(paste0("Custom_comb",i, "_int_w_w"), mod)
+#
+#     save(list = c(paste0("Custom_comb",i, "_int_w_w")), file = paste0("Custom_comb",i, "_int_w_w",'.Rda'))
+# }
+
 for (i in 3:10){
 
     mod = demand_forecast(X_mat, date_demand,forc_start = '2016-01-01', forc_end = '2023-01-01',
-                                      pred_win = 30, pred_lag = 15, train_y = 5, p_comps = i,  no_pc = TRUE,
-custom = paste0("as.factor(hour):as.factor(month)  + as.factor(w_day):as.factor(month) + s(week) + s(month) + season1 + season2 + year + s(PC1)+ s(PC",i,")") ,
-cores = 48, reg_form = "volume~1")
+                          pred_win = 30, pred_lag = 15, train_y = 5, p_comps = i,  no_pc = TRUE,
+                          custom = paste0("as.factor(hour):as.factor(month)  + as.factor(w_day):as.factor(week) + s(week) + s(month) + season1 + season2 + year + s(PC1)+ s(PC",i,")") ,
+                          cores = 48, reg_form = "volume~1")
+    assign(paste0("Custom_comb",i, "_int_m_w"), mod)
+
+    save(list = c(paste0("Custom_comb",i, "_int_m_w")), file = paste0("Custom_comb",i, "_int_m_w",'.Rda'))
+}
+
+for (i in 3:10){
+
+    mod = demand_forecast(X_mat, date_demand,forc_start = '2016-01-01', forc_end = '2023-01-01',
+                          pred_win = 30, pred_lag = 15, train_y = 5, p_comps = i,  no_pc = TRUE,
+                          custom = paste0("as.factor(hour):as.factor(week)  + as.factor(w_day):as.factor(month) + s(week) + s(month) + season1 + season2 + year + s(PC1)+ s(PC",i,")") ,
+                          cores = 48, reg_form = "volume~1")
     assign(paste0("Custom_comb",i, "_int_w_m"), mod)
 
     save(list = c(paste0("Custom_comb",i, "_int_w_m")), file = paste0("Custom_comb",i, "_int_w_m",'.Rda'))
+}
+
+for (i in 3:10){
+
+    mod = demand_forecast(X_mat, date_demand,forc_start = '2016-01-01', forc_end = '2023-01-01',
+                          pred_win = 30, pred_lag = 15, train_y = 5, p_comps = i,  no_pc = TRUE,
+                          custom = paste0("as.factor(hour):as.factor(month)  + as.factor(w_day):as.factor(month) + s(week) + s(month) + season1 + season2 + year + s(PC1)+ s(PC",i,")") ,
+                          cores = 48, reg_form = "volume~1")
+    assign(paste0("Custom_comb",i, "_int_m_m"), mod)
+    save(list = c(paste0("Custom_comb",i, "_int_m_m")), file = paste0("Custom_comb",i, "_int_m_m",'.Rda'))
 }
