57a543c330778fcef0fffc37119a20d550da2322
Eirik Sjåvik <Eirik@eduroam-193-157-170-228.wlan.uio.no>
2023-03-21 17:19:17 +0100

    Added new mod run

--- Dev/run_NWP.R
+++ Dev/run_NWP.R
@@ -19,29 +19,31 @@ for (i in c(1,2,4,8,16,32)){
 #                                        window = 120, cores = 48, formula  = 'PC_32days_roll ~ 1 + NWP1_roll32', incl_climatology = TRUE)
 # save(Mod_9_120_climatology_per32, file = 'Mod_9_120_climatology_per32.Rda')
 
-Mod_9_120_NWP1_per1 = rolling_mod_NWP('2007-01-01', '2023-01-31', 0.9, ERA_NWP, 0, model = 'reg',incl_other = c("PC_1days_roll", "NWP1_roll1"),
-                                       window = 120, cores = 48, formula  = 'PC_1days_roll ~ 1 + NWP1_roll1')
-save(Mod_9_120_NWP1_per1, file = 'Mod_9_120_NWP1_per1.Rda')
-
-Mod_9_120_NWP1_per2 = rolling_mod_NWP('2007-01-01', '2023-01-31', 0.9, ERA_NWP, 0, model = 'reg',incl_other = c("PC_2days_roll", "NWP1_roll2"),
-                                       window = 120, cores = 48, formula  = 'PC_2days_roll ~ 1 + NWP1_roll2')
-save(Mod_9_120_NWP1_per2, file = 'Mod_9_120_NWP1_per2.Rda')
-
-Mod_9_120_NWP1_per4 = rolling_mod_NWP('2007-01-01', '2023-01-31', 0.9, ERA_NWP, 0, model = 'reg',incl_other = c("PC_4days_roll", "NWP1_roll4"),
-                                       window = 120, cores = 48, formula  = 'PC_4days_roll ~ 1 + NWP1_roll4')
-save(Mod_9_120_NWP1_per4, file = 'Mod_9_120_NWP1_per4.Rda')
-
-Mod_9_120_NWP1_per8 = rolling_mod_NWP('2007-01-01', '2023-01-31', 0.9, ERA_NWP, 0, model = 'reg',incl_other = c("PC_8days_roll", "NWP1_roll8"),
-                                       window = 120, cores = 48, formula  = 'PC_8days_roll ~ 1 + NWP1_roll8')
-save(Mod_9_120_NWP1_per8, file = 'Mod_9_120_NWP1_per8.Rda')
-
-Mod_9_120_NWP1_per16 = rolling_mod_NWP('2007-01-01', '2023-01-31', 0.9, ERA_NWP, 0, model = 'reg',incl_other = c("PC_16days_roll", "NWP1_roll16"),
-                                       window = 120, cores = 48, formula  = 'PC_16days_roll ~ 1 + NWP1_roll16')
-save(Mod_9_120_NWP1_per16, file = 'Mod_9_120_NWP1_per16.Rda')
+# Mod_9_120_NWP1_per1 = rolling_mod_NWP('2007-01-01', '2023-01-31', 0.9, ERA_NWP, 0, model = 'reg',incl_other = c("PC_1days_roll", "NWP1_roll1"),
+#                                        window = 120, cores = 48, formula  = 'PC_1days_roll ~ 1 + NWP1_roll1')
+# save(Mod_9_120_NWP1_per1, file = 'Mod_9_120_NWP1_per1.Rda')
+#
+# Mod_9_120_NWP1_per2 = rolling_mod_NWP('2007-01-01', '2023-01-31', 0.9, ERA_NWP, 0, model = 'reg',incl_other = c("PC_2days_roll", "NWP1_roll2"),
+#                                        window = 120, cores = 48, formula  = 'PC_2days_roll ~ 1 + NWP1_roll2')
+# save(Mod_9_120_NWP1_per2, file = 'Mod_9_120_NWP1_per2.Rda')
+#
+# Mod_9_120_NWP1_per4 = rolling_mod_NWP('2007-01-01', '2023-01-31', 0.9, ERA_NWP, 0, model = 'reg',incl_other = c("PC_4days_roll", "NWP1_roll4"),
+#                                        window = 120, cores = 48, formula  = 'PC_4days_roll ~ 1 + NWP1_roll4')
+# save(Mod_9_120_NWP1_per4, file = 'Mod_9_120_NWP1_per4.Rda')
+#
+# Mod_9_120_NWP1_per8 = rolling_mod_NWP('2007-01-01', '2023-01-31', 0.9, ERA_NWP, 0, model = 'reg',incl_other = c("PC_8days_roll", "NWP1_roll8"),
+#                                        window = 120, cores = 48, formula  = 'PC_8days_roll ~ 1 + NWP1_roll8')
+# save(Mod_9_120_NWP1_per8, file = 'Mod_9_120_NWP1_per8.Rda')
+#
+# Mod_9_120_NWP1_per16 = rolling_mod_NWP('2007-01-01', '2023-01-31', 0.9, ERA_NWP, 0, model = 'reg',incl_other = c("PC_16days_roll", "NWP1_roll16"),
+#                                        window = 120, cores = 48, formula  = 'PC_16days_roll ~ 1 + NWP1_roll16')
+# save(Mod_9_120_NWP1_per16, file = 'Mod_9_120_NWP1_per16.Rda')
 
 Mod_9_120_NWP1_per32 = rolling_mod_NWP('2007-01-01', '2023-01-31', 0.9, ERA_NWP, 0, model = 'reg',incl_other = c("PC_32days_roll", "NWP1_roll32"),
                                 window = 120, cores = 48, formula  = 'PC_32days_roll ~ 1 + NWP1_roll32')
-save(Mod_9_120_NWP1_per32, file = 'Mod_9_120_NWP1_per32.Rda')vim
+save(Mod_9_120_NWP1_per32, file = 'Mod_9_120_NWP1_per32.Rda')
 
 # model = 'reg';incl_other = c("NWP1_roll1","PC_1days_roll");
 # window = 120; cores = 48; formula  = 'PC_1days_roll ~ 1 + NWP1_roll1'
+
+scp  eirikhsj@abacus-as.uio.no:~/Mod_9_120_NWP1_per1.Rda eirikhsj@abacus-as.uio.no:~/Mod_9_120_NWP1_per2.Rda  eirikhsj@abacus-as.uio.no:~/Mod_9_120_NWP1_per4.Rda  eirikhsj@abacus-as.uio.no:~/Mod_9_120_NWP1_per8.Rda  eirikhsj@abacus-as.uio.no:~/Mod_9_120_NWP1_per16.Rda  eirikhsj@abacus-as.uio.no:~/Mod_9_120_NWP1_per32.Rda /Users/Eirik/Desktop/Master2023/Output_data/
