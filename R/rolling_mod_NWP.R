6e1b6e98139b6631db6ab4d02886c07eae3e19d3
Eirik Sjåvik <Eirik@eduroam-193-157-251-188.wlan.uio.no>
2023-03-31 08:44:25 +0200

    Added function rolling_mod_NWP_interval.R

--- R/rolling_mod_NWP.R
+++ R/rolling_mod_NWP.R
@@ -23,8 +23,8 @@
 #' @name rolling_mod_NWP
 
 
-forc_start=as.Date('2007-01-01'); forc_end=as.Date('2023-01-01'); q=0.9; ERA_NWP; predictors=0; model='qreg'; window = 125; reweight = FALSE;
-incl_climatology =TRUE; formula = 'PC1 ~ 1'; incl_other = FALSE; skill_interval = 0; cores = 4
+# forc_start=as.Date('2007-01-01'); forc_end=as.Date('2023-01-01'); q=0.9; ERA_NWP; predictors=0; model='qreg'; window = 125; reweight = FALSE;
+# incl_climatology =TRUE; formula = 'PC1 ~ 1'; incl_other = FALSE; skill_interval = 0; cores = 4
 
 #' @export
 rolling_mod_NWP = function(forc_start=as.Date('2007-01-01'), forc_end=as.Date('2023-01-01'), q=0.9, ERA_NWP, predictors=1, model='qreg', window = 60, reweight = FALSE,
